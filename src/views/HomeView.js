37c164525fe7295fdff6b9ff2a4898ed7bb716c5
shakiroorakam <shakiroorakam@gmail.com>
2025-09-02 12:29:24 +0530

    home ui update

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -7,17 +7,19 @@ export function HomeView() {
     <div className='home-card'>
       <h1>Live Quiz</h1>
       <p>Choose your role to begin</p>
-      {/* Using align-items-stretch to ensure both buttons have the same width */}
-      <div
-        className='d-flex flex-column align-items-stretch'
-        style={{ gap: "1rem" }}
-      >
-        <Link to='/admin' className='home-btn home-btn-admin'>
-          <ShieldCheck className='mr-2' size={22} /> Admin Login
-        </Link>
-        <Link to='/join' className='home-btn home-btn-join'>
-          <UserCheck className='mr-2' size={22} /> Join Quiz
-        </Link>
+      {/* This new structure ensures the buttons are centered and have a consistent width */}
+      <div className='d-flex justify-content-center'>
+        <div
+          className='d-flex flex-column align-items-stretch'
+          style={{ gap: "1rem", width: "100%", maxWidth: "250px" }}
+        >
+          <Link to='/admin' className='home-btn home-btn-admin'>
+            <ShieldCheck className='mr-2' size={22} /> Admin Login
+          </Link>
+          <Link to='/join' className='home-btn home-btn-join'>
+            <UserCheck className='mr-2' size={22} /> Join Quiz
+          </Link>
+        </div>
       </div>
     </div>
   );
